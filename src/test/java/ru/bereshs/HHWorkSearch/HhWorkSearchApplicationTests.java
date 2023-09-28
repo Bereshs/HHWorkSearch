f464ad668cee9f47b940d29534c6dfa3c470e42b
Sergey Bereshpolov <bereshs@mail.ru>
2023-09-28 10:15:20 +0300

    initializations

---
+++ src/test/java/ru/bereshs/HHWorkSearch/HhWorkSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bereshs.HHWorkSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhWorkSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
