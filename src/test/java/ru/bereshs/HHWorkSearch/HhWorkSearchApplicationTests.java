30b079ab3808ae6f7be8a0d7beb4cdaa21167951
Sergey Bereshpolov <bereshs@mail.ru>
2024-04-27 16:49:26 +0300

    disable spring boot test

--- src/test/java/ru/bereshs/HHWorkSearch/HhWorkSearchApplicationTests.java
+++ src/test/java/ru/bereshs/HHWorkSearch/HhWorkSearchApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("Test")
-@SpringBootTest("spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration")
+@SpringBootTest
 class HhWorkSearchApplicationTests {
 
 	@Test
